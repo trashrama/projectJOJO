d795c8c7ae316c89ace03b731ecd7f9407cc815e
trashrama <canaisdosantiago@gmail.com>
2023-01-29 10:05:28 -0300

    quase tudo funcionando

--- app.py
+++ app.py
@@ -24,49 +24,53 @@ def close_cookie_ad(navegador):
 def get_info(navegador, STANDS_DB, PARTE):
     stand_info = []
 
-    # adiciona o nome do stand na lista
-    name_stand = navegador.find_element(
-        'xpath', "//h2[@class='pi-item pi-item-spacing pi-title']").text
-    # adiciona o nome do usuario
-    div_stand_master = navegador.find_elements(
-        'xpath', '//div[@data-source="user"]')[0].text
-    div_stand_master = div_stand_master.split("\n")
-    stand_master = div_stand_master[1]
+    #fazer um try pra cada caso
 
-    print(navegador.find_element(
-        'xpath', "//td[@data-source='destpower']").get_attribute("innerHTML"))
+    try: 
+        # adiciona o nome do stand na lista
+        name_stand = navegador.find_element(
+            'xpath', "//h2[@class='pi-item pi-item-spacing pi-title']").text
+        # adiciona o nome do usuario
+        div_stand_master = navegador.find_elements(
+            'xpath', '//div[@data-source="user"]')[0].text
+        div_stand_master = div_stand_master.split("\n")
+        stand_master = div_stand_master[1]
 
-    lista_hab = [navegador.find_element(
-        'xpath', "//td[@data-source='destpower']").get_attribute("innerHTML"), navegador.find_element(
-        'xpath', "//td[@data-source='speed']").get_attribute("innerHTML"), navegador.find_element(
-        'xpath', "//td[@data-source='range']").get_attribute("innerHTML"),
-        navegador.find_element(
-        'xpath', "//td[@data-source='stamina']").get_attribute("innerHTML"), navegador.find_element(
-        'xpath', "//td[@data-source='precision']").get_attribute("innerHTML"), navegador.find_element(
-        'xpath', "//td[@data-source='potential']").get_attribute("innerHTML")]
+        print(navegador.find_element(
+            'xpath', "//td[@data-source='destpower']").get_attribute("innerHTML"))
 
-    stand_info.append(name_stand)
-    stand_info.append(PARTE)
-    stand_info.append(stand_master)
-    stand_info.extend(lista_hab)
-    STANDS_DB.append(stand_info)
+        lista_hab = [navegador.find_element(
+            'xpath', "//td[@data-source='destpower']").get_attribute("innerHTML"), navegador.find_element(
+            'xpath', "//td[@data-source='speed']").get_attribute("innerHTML"), navegador.find_element(
+            'xpath', "//td[@data-source='range']").get_attribute("innerHTML"),
+            navegador.find_element(
+            'xpath', "//td[@data-source='stamina']").get_attribute("innerHTML"), navegador.find_element(
+            'xpath', "//td[@data-source='precision']").get_attribute("innerHTML"), navegador.find_element(
+            'xpath', "//td[@data-source='potential']").get_attribute("innerHTML")]
 
+        stand_info.append(name_stand)
+        stand_info.append(PARTE)
+        stand_info.append(stand_master)
+        stand_info.extend(lista_hab)
+        STANDS_DB.append(stand_info)
+    except:
+        pass
 
 navegador = webdriver.Chrome(executable_path=r'chromedriver')
 
 navegador.get("https://jojowiki.com/List_of_Stands")
 
-
-indice_artigos = 1
+i = 1
 while True:
 
-    pagina = WebDriverWait(navegador, 5).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="tabber-7b41f183054568e35f06bd9e4c65aa1d"]/div[1]/div[1]/div/div[2]/div[{}]/div[2]/a'.format(indice_artigos))))
+    WebDriverWait(navegador, 3)
+    lista_paginas = navegador.find_element(
+        'xpath', "(//div[@class='charname'])[{}]".format(i))
+    lista_paginas.click()
+    i = i + 1
 
-    pagina.click()
     get_info(navegador, STANDS_DB, JOJO_PARTS[0])
     print(STANDS_DB)
     navegador.back()
 
-    indice_artigos = indice_artigos + 1
+
